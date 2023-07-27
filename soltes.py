10394fd93692071890dfe057d8734c0c67df2740
Samuel <xsolte02@stud.fit.vutbr.cz>
2023-07-27 05:15:47 +0200

    initial commit

---
+++ soltes.py
@@ -0,0 +1,19 @@
+"""
+Retailys coding test 2023
+Samuel Šoltés
+27.7.2023  
+
+"""
+
+
+def process_zip():
+    pass
+    # source: https://www.retailys.cz/wp-content/uploads/astra_export_xml.zip
+    # docs: https://www.astramodel.cz/b2b/export.pdf
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

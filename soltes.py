f774500270e4195f6a398510076e20bff2e0860e
Samuel <xsolte02@stud.fit.vutbr.cz>
2023-07-27 06:47:55 +0200

    zip download & processing

--- soltes.py
+++ soltes.py
@@ -5,15 +5,41 @@ Samuel Šoltés
 
 """
 
+import requests, zipfile, io
 
-def process_zip():
+def process_zip(zip_url : str, extraction_path : str = r"./") -> str:
+
+    req = requests.get(zip_url)
+    if not req.ok:
+        raise Exception("Request failed.")
+     
+    with zipfile.ZipFile(io.BytesIO(req.content)) as my_zip:
+        my_zip.extractall(extraction_path)
+        xml_name = my_zip.infolist()[0].filename
+
+    return xml_name
+
+def count_products():
     pass
-    # source: https://www.retailys.cz/wp-content/uploads/astra_export_xml.zip
-    # docs: https://www.astramodel.cz/b2b/export.pdf
+
+
+def product_names():
+    pass
+
+
+def spare_parts():
+    pass
+
 
 def main():
-    # comment
+    # 1) how many products there are
+    # 2) print product names
+    # 3) print spare parts
+
     pass
+    zip_source = "https://www.retailys.cz/wp-content/uploads/astra_export_xml.zip"
+
+    process_zip(zip_source)
 
 
 if __name__ == "__main__":
